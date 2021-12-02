b40db1f4e21472c19a7f249a8dc41c146b893fc0
stevenhuyn <stevenhuyn@gmail.com>
2021-12-02 00:58:57 -0800

    Solve day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+
+mod day1;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let day = args[1].parse::<i32>().unwrap();
+    let part = args[2].chars().next().unwrap();
+    run(day, part);
+}
+
+fn run(day: i32, part: char) {
+    match day {
+        1 => day1::run(part),
+        _ => println!("bruh"),
+    };
 }
