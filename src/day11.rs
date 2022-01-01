737ac6915653da37411933cba3705deb5bd203ec
stevenhuyn <stevenhuyn@gmail.com>
2021-12-31 20:33:07 -0800

    Finish day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -7,68 +7,80 @@ use itertools::Itertools;
 
 fn solve_p1() {
     let mut grid = read_input("input/day11.txt").unwrap();
-    println!("{:?}", grid);
-
-    let xlen = grid.len();
-    let ylen = grid[0].len();
 
     let mut flash_count = 0;
 
     for _ in 0..100 {
-        let mut queue: Vec<(usize, usize)> = Vec::new();
-        let mut seen: HashSet<(usize, usize)> = HashSet::new();
-
-        // Increment everything by 1 then add to queue if a 9
-        grid.iter_mut().enumerate().for_each(|(i, row)| {
-            row.iter_mut().enumerate().for_each(|(j, v)| {
-                *v += 1;
-                if *v > 9 {
-                    queue.push((i, j));
-                    seen.insert((i, j));
-                }
-            })
-        });
-
-        // Now BFS the frick out of everything
-        while !queue.is_empty() {
-            let flash_octo = queue.pop().unwrap();
-            let (x, y) = flash_octo;
-
-            // get neighbours, flash and push new octopi to 9s in queue
-            for (dx, dy) in [-1i32, 0, 1].into_iter().cartesian_product([-1i32, 0, 1]) {
-                if x as i32 + dx < 0 || y as i32 + dy < 0 {
-                    continue;
-                }
-
-                let nx = (x as i32 + dx) as usize;
-                let ny = (y as i32 + dy) as usize;
-                if (nx, ny) != flash_octo && nx < xlen && ny < ylen {
-                    grid[nx][ny] += 1;
-                    if !seen.contains(&(nx, ny)) && grid[nx][ny] > 9 {
-                        queue.push((nx, ny));
-                        seen.insert((nx, ny));
-                    }
-                }
-            }
-        }
-
-        // Now convert all flashed octopi to 0
-        grid.iter_mut().for_each(|row| {
-            row.iter_mut().for_each(|v| {
-                if *v > 9 {
-                    *v = 0
-                }
-            })
-        });
-
-        flash_count += seen.len();
+        flash_count += iterate(&mut grid);
     }
 
     println!("flash count: {}", flash_count);
 }
 
 fn solve_p2() {
-    println!("p2 answer")
+    let mut grid = read_input("input/day11.txt").unwrap();
+
+    let octopus_count = (grid.len() * grid[0].len()) as i32;
+
+    for i in 1.. {
+        if iterate(&mut grid) == octopus_count {
+            println!("First all flash: {}", i);
+            break;
+        }
+    }
+}
+
+fn iterate(grid: &mut Vec<Vec<usize>>) -> i32 {
+    let mut queue: Vec<(usize, usize)> = Vec::new();
+    let mut seen: HashSet<(usize, usize)> = HashSet::new();
+
+    let xlen = grid.len();
+    let ylen = grid[0].len();
+
+    // Increment everything by 1 then add to queue if a 9
+    grid.iter_mut().enumerate().for_each(|(i, row)| {
+        row.iter_mut().enumerate().for_each(|(j, v)| {
+            *v += 1;
+            if *v > 9 {
+                queue.push((i, j));
+                seen.insert((i, j));
+            }
+        })
+    });
+
+    // Now BFS the frick out of everything
+    while !queue.is_empty() {
+        let flash_octo = queue.pop().unwrap();
+        let (x, y) = flash_octo;
+
+        // get neighbours, flash and push new octopi to 9s in queue
+        for (dx, dy) in [-1i32, 0, 1].into_iter().cartesian_product([-1i32, 0, 1]) {
+            if x as i32 + dx < 0 || y as i32 + dy < 0 {
+                continue;
+            }
+
+            let nx = (x as i32 + dx) as usize;
+            let ny = (y as i32 + dy) as usize;
+            if (nx, ny) != flash_octo && nx < xlen && ny < ylen {
+                grid[nx][ny] += 1;
+                if !seen.contains(&(nx, ny)) && grid[nx][ny] > 9 {
+                    queue.push((nx, ny));
+                    seen.insert((nx, ny));
+                }
+            }
+        }
+    }
+
+    // Now convert all flashed octopi to 0
+    grid.iter_mut().for_each(|row| {
+        row.iter_mut().for_each(|v| {
+            if *v > 9 {
+                *v = 0
+            }
+        })
+    });
+
+    seen.len() as i32
 }
 
 pub fn run(day: i32) {
