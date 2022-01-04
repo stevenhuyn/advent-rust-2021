15a7ccb0d0d537aa7ab925802382a079a76144be
stevenhuyn <stevenhuyn@gmail.com>
2022-01-04 12:39:51 -0800

    Solve day 14 part 1

--- src/day14.rs
+++ src/day14.rs
@@ -3,15 +3,50 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::time::Instant;
 
-fn solve_p1() {
-    let (seed, instructions) = read_input("input/test14.txt").unwrap();
+use itertools::{enumerate, Itertools};
 
-    println!("{:?}, {:?}", seed, instructions);
-    println!("p1 answer")
+fn solve_p1() {
+    solve(10);
 }
 
 fn solve_p2() {
-    println!("p2 answer")
+    solve(40);
+}
+
+fn solve(steps: usize) {
+    let (seed, instructions) = read_input("input/day14.txt").unwrap();
+
+    let mut curr: Vec<char> = seed.chars().collect();
+    let mut new = curr.clone();
+    for _ in 0..steps {
+        let mut inserted_count = 0;
+
+        for (i, pair) in enumerate(curr.into_iter().tuple_windows::<(char, char)>()) {
+            // Need to use reference iterator because we consume iterator multiple times
+            for (pattern, to_insert) in instructions.iter() {
+                if pair == *pattern {
+                    new.insert(i + 1 + inserted_count, *to_insert);
+                    inserted_count += 1;
+                    break;
+                }
+            }
+        }
+        curr = new.clone();
+    }
+
+    let counts: Vec<usize> = curr
+        .iter()
+        .unique()
+        .map(|c| curr.iter().filter(|&v| v == c).count())
+        .collect();
+
+    println!("curr len: {}", curr.len());
+    println!("counts: {:?}", counts);
+
+    println!(
+        "Answer: {}",
+        counts.iter().max().unwrap() - counts.iter().min().unwrap()
+    );
 }
 
 pub fn run(day: i32) {
@@ -24,20 +59,24 @@ pub fn run(day: i32) {
     println!("Ran in {}", now.elapsed().as_secs_f64());
 }
 
-pub fn read_input(filename: &str) -> Result<(String, Vec<(String, String)>), Box<dyn Error>> {
+pub fn read_input(filename: &str) -> Result<(String, Vec<((char, char), char)>), Box<dyn Error>> {
     let file = File::open(filename)?;
     let bufreader = BufReader::new(file);
     let mut lines = bufreader.lines();
 
     let seed = lines.next().unwrap().unwrap();
 
-    let mut instructions: Vec<(String, String)> = Vec::new();
+    let mut instructions: Vec<((char, char), char)> = Vec::new();
     for line_res in lines.skip(1) {
         let line = line_res.unwrap();
         let mut split = line.split(" -> ");
         let lhs = split.next().unwrap().to_owned();
+        let mut lhs_iter = lhs.chars();
         let rhs = split.next().unwrap().to_owned();
-        instructions.push((lhs, rhs));
+        instructions.push((
+            (lhs_iter.next().unwrap(), lhs_iter.next().unwrap()),
+            rhs.chars().next().unwrap(),
+        ));
     }
 
     Ok((seed, instructions))
