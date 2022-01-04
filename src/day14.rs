48486efd2d580a6c336cbd6f51d434da12ec6304
stevenhuyn <stevenhuyn@gmail.com>
2022-01-04 15:22:31 -0800

    Finish day 14 part 2

--- src/day14.rs
+++ src/day14.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -6,19 +7,11 @@ use std::time::Instant;
 use itertools::{enumerate, Itertools};
 
 fn solve_p1() {
-    solve(10);
-}
-
-fn solve_p2() {
-    solve(40);
-}
-
-fn solve(steps: usize) {
     let (seed, instructions) = read_input("input/day14.txt").unwrap();
 
     let mut curr: Vec<char> = seed.chars().collect();
     let mut new = curr.clone();
-    for _ in 0..steps {
+    for _ in 0..10 {
         let mut inserted_count = 0;
 
         for (i, pair) in enumerate(curr.into_iter().tuple_windows::<(char, char)>()) {
@@ -43,6 +36,75 @@ fn solve(steps: usize) {
     println!("curr len: {}", curr.len());
     println!("counts: {:?}", counts);
 
+    println!(
+        "Answer: {}",
+        counts.iter().max().unwrap() - counts.iter().min().unwrap()
+    );
+}
+
+fn solve_p2() {
+    let (seed, instructions) = read_input("input/day14.txt").unwrap();
+
+    let mut pair_counter: HashMap<(char, char), i64> = HashMap::new();
+    let mut unique_chars: HashSet<char> = HashSet::new();
+
+    for char in seed.chars() {
+        unique_chars.insert(char);
+    }
+
+    for pair in seed.chars().into_iter().tuple_windows::<(char, char)>() {
+        *pair_counter.entry(pair).or_insert(0) += 1;
+    }
+
+    for _ in 0..40 {
+        let mut counter_delta: HashMap<(char, char), i64> = HashMap::new();
+        for (&pair, &amount) in pair_counter.iter() {
+            for &(pattern, to_insert) in instructions.iter() {
+                if pair == pattern {
+                    unique_chars.insert(to_insert);
+                    *counter_delta.entry(pair).or_insert(0) -= amount;
+                    *counter_delta.entry((pair.0, to_insert)).or_insert(0) += amount;
+                    *counter_delta.entry((to_insert, pair.1)).or_insert(0) += amount;
+                    break;
+                }
+            }
+        }
+
+        for (pair, delta) in counter_delta {
+            let entry = pair_counter.entry(pair).or_insert(0);
+            *entry += delta;
+            if *entry == 0 {
+                pair_counter.remove(&pair);
+            }
+        }
+    }
+
+    let mut counts: Vec<i64> = Vec::new();
+    for char in unique_chars {
+        let mut l_count = 0;
+        let mut r_count = 0;
+        for ((l, r), amount) in pair_counter.iter() {
+            if *l == char {
+                l_count += amount;
+            }
+
+            if *r == char {
+                r_count += amount;
+            }
+        }
+
+        if seed.chars().next().unwrap() == char {
+            l_count += 1;
+        }
+
+        if seed.chars().last().unwrap() == char {
+            r_count += 1;
+        }
+
+        counts.push((l_count + r_count) / 2);
+    }
+
+    println!("counts {:?}", counts);
     println!(
         "Answer: {}",
         counts.iter().max().unwrap() - counts.iter().min().unwrap()
