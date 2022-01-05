fd42d633efe4add03f8422a86ff0042753df7778
stevenhuyn <stevenhuyn@gmail.com>
2022-01-05 12:02:35 -0800

    Parse literal case

--- src/day16.rs
+++ src/day16.rs
@@ -7,20 +7,47 @@ fn solve_p1() {
     let hex_packet = read_input("input/test16.txt").unwrap();
     let bits = packet_to_bits(&hex_packet);
     println!("{} {:?}", hex_packet, bits);
+
+    recurse_packet(&bits);
 }
 
 fn solve_p2() {
     println!("p2 answer")
 }
 
-fn packet_to_bits(hex_packet: &str) -> Vec<u32> {
+fn recurse_packet(packet: &str) {
+    let version = usize::from_str_radix(&packet[..3], 2).unwrap();
+    let type_id = usize::from_str_radix(&packet[3..6], 2).unwrap();
+
+    match type_id {
+        4 => {
+            // Literal value
+            let literal_string = build_literal(&packet[6..]);
+            let literal = usize::from_str_radix(&literal_string, 2).unwrap();
+            println!("literal: {}", literal);
+        }
+        _ => {}
+    }
+
+    println!("version: {}, type: {}", version, type_id);
+}
+
+fn build_literal(literal: &str) -> String {
+    let mut answer = "".to_owned();
+    answer.push_str(&literal[1..5]);
+
+    if literal[0..1] == *"1" {
+        answer.push_str(&build_literal(&literal[5..]));
+    }
+
+    answer
+}
+
+fn packet_to_bits(hex_packet: &str) -> String {
     let value = u32::from_str_radix(hex_packet, 16).unwrap();
     let binary_string = format!("{:b}", value);
 
     binary_string
-        .chars()
-        .map(|c| c.to_digit(10).unwrap())
-        .collect()
 }
 
 pub fn run(day: i32) {
