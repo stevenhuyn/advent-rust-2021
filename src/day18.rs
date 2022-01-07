ed14ba12a9d7a713170b327dbd0253700b4cae07
stevenhuyn <stevenhuyn@gmail.com>
2022-01-06 19:36:41 -0800

    Rudimentary split

--- src/day18.rs
+++ src/day18.rs
@@ -5,7 +5,7 @@ use std::time::Instant;
 #[derive(Debug)]
 enum SnailNode {
     Value(i32),
-    Tree(Box<SnailNode>, Box<SnailNode>),
+    Node(Box<SnailNode>, Box<SnailNode>),
 }
 #[derive(Debug)]
 struct SnailNum {
@@ -14,7 +14,7 @@ struct SnailNum {
 
 impl SnailNum {
     fn print(&self) {
-        println!("{:?}", self);
+        println!("{:?}", &self);
     }
 
     fn read_tree(input: &str) -> SnailNum {
@@ -29,7 +29,7 @@ impl SnailNum {
 
     fn read_nodes(input: &str) -> SnailNode {
         if let Some(mid_comma_ind) = SnailNum::find_middle_comma(input) {
-            SnailNode::Tree(
+            SnailNode::Node(
                 Box::new(SnailNum::read_nodes(&input[1..mid_comma_ind])),
                 Box::new(SnailNum::read_nodes(
                     &input[mid_comma_ind + 1..input.len() - 1],
@@ -66,28 +66,67 @@ impl SnailNum {
             if v >= 10 {
                 // SPLIT
                 let half = v as f32 / 2f32;
-                return SnailNode::Tree(
+                return SnailNode::Node(
                     Box::new(SnailNode::Value(half.ceil() as i32)),
                     Box::new(SnailNode::Value(half.floor() as i32)),
                 );
             }
-        } else if let SnailNode::Tree(box left, box right) = node {
-            if depth >= 5 {
-                // EXPLODE
-            }
-            node
         }
+        // } else if let SnailNode::Node(box left, box right) = node {
+        // if depth >= 5 {
+        //     // EXPLODE
+        // }
+        // node
+        // }
 
         node
+    }
+
+    fn split(node: &mut SnailNode) -> bool {
+        if let SnailNode::Value(v) = node {
+            if *v >= 5 {
+                // SPLIT
+                let half = *v as f32 / 2f32;
+                *node = SnailNode::Node(
+                    Box::new(SnailNode::Value(half.ceil() as i32)),
+                    Box::new(SnailNode::Value(half.floor() as i32)),
+                );
+                return true;
+            }
+
+            false
+        } else if let SnailNode::Node(left, right) = node {
+            SnailNum::split(left) || SnailNum::split(right)
+        } else {
+            panic!("??")
+        }
+    }
+
+    fn traverse(node: SnailNode) {
+        if let SnailNode::Value(v) = node {
+            print!("{} ", v);
+        } else if let SnailNode::Node(left, right) = node {
+            SnailNum::traverse(*left);
+            SnailNum::traverse(*right);
+        }
     }
 }
 
 fn solve_p1() {
-    let snail_nums = read_input("input/test18.txt");
+    // let snail_nums = read_input("input/test18.txt");
 
-    for snail_num in snail_nums {
-        snail_num.print();
-    }
+    // for snail_num in snail_nums {
+    //     let root = snail_num.root.unwrap();
+    //     println!();
+    //     SnailNum::traverse(root);
+    // }
+
+    let mut snail_num = SnailNum::read_tree("[10,10]");
+
+    snail_num.print();
+    let root = snail_num.root.as_mut().unwrap();
+    SnailNum::split(root);
+    snail_num.print();
 
     println!("p1 answer")
 }
