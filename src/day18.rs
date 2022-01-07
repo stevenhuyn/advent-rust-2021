e6f81d2d24ea5fb8131a556fbcd4491d52743eaa
stevenhuyn <stevenhuyn@gmail.com>
2022-01-06 17:34:12 -0800

    Failed attempt day 18

--- src/day18.rs
+++ src/day18.rs
@@ -1,16 +1,19 @@
-use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::time::Instant;
 
-#[derive(Debug)]
-struct SnailNum {
+use itertools::Itertools;
+
+#[derive(Debug, Copy, Clone)]
+struct RegularNum {
     value: i32,
     depth: i32,
     side: Side,
 }
 
-#[derive(Debug, Copy, Clone)]
+type SnailNum = Vec<RegularNum>;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
 
 enum Side {
     Left,
@@ -19,13 +22,163 @@ enum Side {
 
 fn solve_p1() {
     let snail_lines = read_input("input/test18.txt");
-    println!("{:?}", snail_lines);
-    println!("p1 answer")
+
+    // println!("Before: {:?}", snail_lines);
+    let sol = snail_lines
+        .into_iter()
+        .reduce(|a, b| reduce(add(a, b)))
+        .unwrap();
+
+    println!();
+
+    println!("Solution len {:?}", sol.len());
+    println!("Solution: {:?}", sol);
+
+    println!();
+    println!("magnitude: {}", magnitude(sol));
 }
 
-fn solve_p2() {
-    println!("p2 answer")
+#[derive(Debug, PartialEq)]
+enum SnailState {
+    Explode(usize, usize),
+    Split(usize),
+    Reduced,
 }
+
+fn magnitude(mut snail_num: SnailNum) -> i32 {
+    while snail_num.len() != 1 {
+        // println!("length: {}", snail_num.len());
+        // for _ in 0..4 {
+        for (i, (left, right)) in snail_num.iter().tuple_windows().enumerate() {
+            if left.side == Side::Left && right.side == Side::Right && left.depth == right.depth {
+                println!("To Magnitude");
+                println!("{:?}", left);
+                println!("{:?}", right);
+
+                snail_num[i].depth -= 1;
+                snail_num[i].value = snail_num[i].value * 3 + snail_num[i + 1].value * 2;
+                snail_num.remove(i + 1);
+                snail_num[i].side = get_side(&snail_num, i);
+                break;
+            }
+        }
+    }
+
+    snail_num[0].value
+}
+
+fn add(left: SnailNum, right: SnailNum) -> SnailNum {
+    println!("Add!");
+    let mut sum = left;
+    sum.extend(right);
+    sum.iter_mut().for_each(|reg_num| reg_num.depth += 1);
+    sum
+}
+
+fn get_side(snail_num: &SnailNum, i: usize) -> Side {
+    if (i >= 1
+        && snail_num[i - 1].side == Side::Left
+        && snail_num[i - 1].depth == snail_num[i].depth)
+        || i == snail_num.len() - 1
+    {
+        return Side::Right;
+    }
+
+    if (i < snail_num.len() - 1
+        && snail_num[i + 1].side == Side::Right
+        && snail_num[i + 1].depth == snail_num[i].depth)
+        || i == 0
+    {
+        return Side::Left;
+    }
+
+    println!("{} {:?}", i, snail_num);
+    panic!("No Side");
+}
+
+fn reduce(mut snail_num: SnailNum) -> SnailNum {
+    let mut snail_state = SnailState::Reduced;
+    loop {
+        // Apply explosion
+        if let SnailState::Explode(left_i, right_i) = snail_state {
+            if left_i >= 1 {
+                snail_num[left_i - 1].value += snail_num[left_i].value;
+            }
+
+            if right_i <= snail_num.len() - 2 {
+                snail_num[right_i + 1].value += snail_num[right_i].value;
+            }
+
+            snail_num.remove(right_i);
+
+            // println!();
+            // println!("{:?} {:?}", left_i, snail_num);
+
+            snail_num[left_i].depth -= 1;
+            snail_num[left_i].value = 0;
+            snail_num[left_i].side = get_side(&snail_num, left_i);
+
+            snail_state = SnailState::Reduced;
+        // Apply Split
+        } else if let SnailState::Split(i) = snail_state {
+            let splitting = snail_num[i];
+            let left_value = splitting.value / 2;
+            let mut right_value = splitting.value / 2;
+            if splitting.value % 2 != 0 {
+                right_value += 1;
+            }
+
+            snail_num.remove(i);
+            snail_num.insert(
+                i,
+                RegularNum {
+                    value: left_value,
+                    depth: splitting.depth + 1,
+                    side: Side::Left,
+                },
+            );
+            snail_num.insert(
+                i + 1,
+                RegularNum {
+                    value: right_value,
+                    depth: splitting.depth + 1,
+                    side: Side::Right,
+                },
+            );
+            snail_state = SnailState::Reduced;
+        }
+
+        for (i, (left, right)) in snail_num.iter().tuple_windows().enumerate() {
+            if left.side == Side::Left
+                && right.side == Side::Right
+                && left.depth == right.depth
+                && left.depth > 4
+            {
+                // Explode!
+                println!("Explode");
+                snail_state = SnailState::Explode(i, i + 1);
+                break;
+            }
+        }
+
+        for (i, regular_num) in snail_num.iter().enumerate() {
+            if regular_num.value >= 10 {
+                // Split!
+                println!("Split");
+                snail_state = SnailState::Split(i);
+                break;
+            }
+        }
+
+        if snail_state == SnailState::Reduced {
+            break;
+        }
+    }
+
+    snail_num
+}
+
+fn solve_p2() {}
 
 pub fn run(day: i32) {
     let now = Instant::now();
@@ -37,7 +190,7 @@ pub fn run(day: i32) {
     println!("Ran in {}", now.elapsed().as_secs_f64());
 }
 
-fn read_input(filename: &str) -> Vec<Vec<SnailNum>> {
+fn read_input(filename: &str) -> Vec<SnailNum> {
     let file = File::open(filename).unwrap();
     let bufreader = BufReader::new(file);
     let lines = bufreader.lines();
@@ -63,7 +216,7 @@ fn read_input(filename: &str) -> Vec<Vec<SnailNum>> {
                             }
                             None
                         })
-                        .map(|value| SnailNum {
+                        .map(|value| RegularNum {
                             value: value as i32,
                             depth,
                             side,
